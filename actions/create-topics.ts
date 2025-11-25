001cb592a883856a3c7ad38ccaca81a438498109
abhaysahu1008@gmail.com <abhaysahugit@gmail.com>
2025-11-25 13:29:08 +0530

    updated

--- actions/create-topics.ts
+++ actions/create-topics.ts
@@ -1,11 +1,11 @@
 'use server'
 import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
+import { Topic } from "@prisma/client";
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
-import { Topic } from "../lib/generated/prisma";
 
 const createTopicSchema = z.object({
   name: z.string().min(3).regex(/^[a-z-]+$/, { message: "Must be lowercase letter without spaces" }),
